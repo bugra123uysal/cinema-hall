9b4ce7fbea6035754b850d71557dcfab56736117
buğra uysal <uysalbugra134@gmail.com>
2024-06-04 23:34:08 +0300

    koltuklar eklendi,kaç numaralı koltuğu aldığın gösteriyor

---
+++ sinema.js
@@ -0,0 +1,24 @@
+var adt=parseFloat( document.getElementById("alışverişticareti").value.length);
+
+function sec(ab){
+   var değeri=alışverişticareti.lenght
+
+   document.getElementById("alışverişticareti").innerHTML+=ab+","
+   document.getElementById("fiyat").innerHTML=değeri*10
+
+
+
+}
+
+
+
+/*  en son billet almayı bitirme buttonu  */
+function tmm(){
+   console.log(adt)
+document.getElementById("fiyat").innerHTML=adt*50
+
+}
+
+
+
+
